6d112bedd1135101a3268c0cda605c2f859f2483
Abhisekh Upadhaya <Abhisek0721@gmail.com>
2024-12-18 18:57:21 +0530

    fix: workspace validation in facebook auth api

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -18,9 +18,9 @@ export class AuthController {
     @Res() res: Response,
   ) {
     await this.authService.validateWorkspace(workspaceId);
-    const facebookAuthUrl = `https://www.facebook.com/v10.0/dialog/oauth?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${process.env.BASE_URL_CONTENT_SERVICE}/api/v1/auth/facebook/callback&state=${workspaceId}&scope=email,public_profile,instagram_basic,instagram_content_publish,pages_show_list,pages_manage_posts`;
+    const redirectAuthUrl = `https://www.facebook.com/v10.0/dialog/oauth?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${process.env.BASE_URL_CONTENT_SERVICE}/api/v1/auth/facebook/callback&state=${workspaceId}&scope=email,public_profile,instagram_basic,instagram_content_publish,pages_show_list,pages_manage_posts`;
     // Redirect to Facebook for authentication
-    return this.apiUtilsService.make_response({ facebookAuthUrl });
+    return this.apiUtilsService.make_response({ redirectAuthUrl });
   }
 
   @Get('facebook/callback')
